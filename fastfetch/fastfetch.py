7e224e60fbdd472a55ae2e09f3cf15fab248ca29
Daniel Massey <masseyd1396@gmail.com>
2025-10-04 17:56:29 +0100

    Strip out terminal ANSI sequences

--- fastfetch/fastfetch.py
+++ fastfetch/fastfetch.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from subprocess import check_output,STDOUT
+from re import compile
 
 class Fastfetch(commands.Cog):
 	def __init__(self, bot):
@@ -10,7 +11,8 @@ class Fastfetch(commands.Cog):
 	async def fastfetch(self, ctx):
 		"Fastfetch plugin by DanielMYT."
 		output = check_output(["bash", "plugins/DanielMYT/modmail-plugins/fastfetch-master/fastfetch"], stderr=STDOUT, timeout=10, text=True)
-		await ctx.send("```\n" + output + "\n```")
+		clean_output = re.compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])').sub('', output)
+		await ctx.send("```\n" + clean_output + "\n```")
 
 async def setup(bot):
 	await bot.add_cog(Fastfetch(bot))
