1e3aacc1b1ae3a3fff8c0ab4a01331553945790b
Daniel Massey <contact@dmassey.net>
2026-08-18 19:00:48 +0100

    Add initial calc plugin

---
+++ calc/calc.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+from subprocess import check_output,STDOUT
+from re import compile
+
+class Calc(commands.Cog):
+        def __init__(self, bot):
+                self.bot = bot
+
+        @commands.command()
+        async def fastfetch(self, ctx, *, expression):
+                "Fast and accurate calculator plugin by DanielMYT using bc."
+                output = check_output(["bash", "plugins/DanielMYT/modmail-plugins/calc/bc.sh", expression], stderr=STDOUT, timeout=3, text=True)
+                plain_output = compile(r'\x1B(?:[@-Z\\-_]|\[[0-?]*[ -/]*[@-~])').sub('', output)
+                await ctx.send(plain_output)
+
+async def setup(bot):
+        await bot.add_cog(Calc(bot))
